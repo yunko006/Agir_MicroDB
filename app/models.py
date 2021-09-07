b01a536d6468a813493e981b410401ca5e43988b
yunko006 <peyrachethomas@gmail.com>
2021-09-07 21:53:47 +0200

    Update models.py
    to change the id

--- app/models.py
+++ app/models.py
@@ -2,6 +2,9 @@ from app import db
 
 
 class Benevole(db.Document):
+    id = db.IntField(primary_key=True)
     nom = db.StringField()
     prenom = db.StringField()
     email = db.StringField()
+
+    meta = {'strict': False}
