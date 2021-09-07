e6d808affa215164d71cacd39a3bd75e1210c0ed
yunko006 <peyrachethomas@gmail.com>
2021-09-07 15:58:51 +0200

    Created several new routes to view all the db, query the db and add a new document

--- app/forms.py
+++ app/forms.py
@@ -2,8 +2,16 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import DataRequired
 
+
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Remember Me')
     submit = SubmitField('Sign In')
+
+
+class BenevoleForm(FlaskForm):
+    nom = StringField('Nom', validators=[DataRequired()])
+    prenom = StringField('Prenom', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired()])
+    submit = SubmitField('Enregistrer')
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
-from flask import render_template, flash, redirect
+from flask import render_template, flash, redirect, url_for, request
 from app import app
-from app.forms import LoginForm
+from app.forms import LoginForm, BenevoleForm
+from app.models import Benevole
 
 
 @app.route('/')
@@ -28,3 +29,35 @@ def login():
             f'Login requested for user {"form.username.data"}, remember_me={form.render_me.data}')
         return redirect(url_for('index'))
     return render_template('login.html', title="Sign In", form=form)
+
+
+@app.route('/benevoles')
+def get_benevoles():
+    benevoles = Benevole.objects()
+    # print(benevoles)
+    # paginated_benevoles = Benevole.objects.paginate(page=page, per_page=10)
+
+    # return render_template('benevoles.html', title='Benevoles', benevoles=benevoles)
+    return render_template('benevoles.html', title='Benevoles', benevoles=benevoles)
+
+
+@app.route('/query')
+def test_query():
+    benevoles = Benevole.objects(nom__icontains="st")
+
+    return render_template('query.html', title='query_test', benevoles=benevoles)
+
+
+@app.route('/register_benevole', methods=['GET', 'POST'])
+def register_benevole():
+    benevole = BenevoleForm()
+    if benevole.validate_on_submit():
+        new_benevole = Benevole()
+        new_benevole.nom = request.form['nom']
+        new_benevole.prenom = request.form['prenom']
+        new_benevole.email = request.form['email']
+        new_benevole.save()
+        flash('Succès ! Nouveau bénévole bien enregistré !')
+        return redirect(url_for('get_benevoles'))
+
+    return render_template('register_benevole.html', title='register_benevole', benevole=benevole)
