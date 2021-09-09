729f6b281dcc5cba277e2f41aa73a0675741eb17
yunko006 <peyrachethomas@gmail.com>
2021-09-09 23:42:02 +0200

    Update utils.py to handle correctly the data
    Will probably needs some changes soon

--- app/utils.py
+++ app/utils.py
@@ -1,6 +1,39 @@
-def appartenance(champs):
+def appartenance(champs: dict):
+    appart = []
     inter = ["volontaire", 'hésitation']
-    domaines_secteurs = ["secteurs", "domaines", "fonctions", "competences"]
+    DomainesEtSecteurs = ["secteurs", "domaines", "fonctions", "compétences"]
+    dispo = ["missions", "projets", "duree_en_mois", "nb_deplacements_par_an"]
+    langues = ["francais", "anglais", "allemand", "espagnol",
+               "italien", "portuguais", "chinois", "russe", "arabe", "autres"]
+    exp_international = ["roles", "experience_inter", "description_exp",
+                         "experience_benevole", "connaissance_structure_inter"]
+    for champ in champs.keys():
+
+        if champ in inter:
+            appart.append("inter")
+
+        elif champ in DomainesEtSecteurs:
+            appart.append("DomainesEtSecteurs")
+
+        elif champ in dispo:
+            appart.append("dispo")
+
+        elif champ in langues:
+            appart.append("langues")
+
+        elif champ in exp_international:
+            appart.append("exp_international")
+
+    return appart
+# resultat = {
+#   'langues__anglais__icontains': 'notions',
+#   'langues__francais__icontains': 'maternelle
+#}
+
+
+def single_appartenance(champs):
+    inter = ["volontaire", 'hésitation']
+    DomainesEtSecteurs = ["secteurs", "domaines", "fonctions", "compétences"]
     dispo = ["missions", "projets", "duree_en_mois", "nb_deplacements_par_an"]
     langues = ["francais", "anglais", "allemand", "espagnol",
                "italien", "portuguais", "chinois", "russe", "arabe", "autres"]
@@ -11,8 +44,8 @@ def appartenance(champs):
     if champs in inter:
         return "inter"
 
-    elif champs in domaines_secteurs:
-        return "domaines_secteurs"
+    elif champs in DomainesEtSecteurs:
+        return "DomainesEtSecteurs"
 
     elif champs in dispo:
         return "dispo"
@@ -38,15 +71,16 @@ def convert_str_to_dict(string):
     return convertedDict
 
 
-def creation_dict(test_dict):
+def creation_dict(test_dict: dict):
     benevole = {}
 
+    appart = appartenance(test_dict)
+    print(appart)
     # create a loop a travers le dictionnaire
     for i, n in enumerate(test_dict):
         # permet d'assigner la valeur des keys a mot clé afin d'executer la query
-        for k in list(test_dict.keys()):
-            appart = (appartenance(k))
+
         # append chaque valeur dans le dict benevole
-        benevole[f"{appart}__{list(test_dict.keys())[i]}__icontains"] = f"{test_dict[n]}"
+        benevole[f"{appart[i]}__{list(test_dict.keys())[i]}__icontains"] = f"{test_dict[n]}"
 
     return benevole
