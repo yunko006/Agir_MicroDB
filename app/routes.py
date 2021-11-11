04144cdd4100521b9cd715598aaf906b1301ab23
yunko006 <peyrachethomas@gmail.com>
2021-11-11 17:20:27 +0100

    Create a file to put unused routes in it, Clean routes.py

--- app/routes.py
+++ app/routes.py
@@ -45,13 +45,6 @@ def get_benevoles():
     return render_template('benevoles.html', title='Benevoles', benevoles=benevoles)
 
 
-@app.route('/query')
-def test_query():
-    benevoles = Benevole.objects(nom__icontains="st")
-
-    return render_template('query.html', title='query_test', benevoles=benevoles)
-
-
 @app.route('/register_benevole', methods=['GET', 'POST'])
 def register_benevole():
     benevole = BenevoleForm()
@@ -116,78 +109,9 @@ def update_benevole():
     return render_template('update_benevole.html', title='Update_bénévole', update=update)
 
 
-@app.route('/recherche', methods=['GET', 'POST'])
-def query():
-    query = QueryForm()
-    if query.validate_on_submit():
-        recherche = request.form['query_field']
-        # faire attention a ce que n soit bien un int
-        n = int(request.form['sliced'])
-
-        benevoles = query_function(recherche, n)
-        print(benevoles)
-
-        return render_template('recherche.html', title='Recherche', benevoles=benevoles)
-
-    return render_template('recherche_form.html', title='Recherche', query=query)
-
-
 @app.route('/help')
 def help():
     return render_template('help.html', title='Aide/FAQ')
-
-
-@app.route('/drop_down', methods=['GET', 'POST'])
-def drop_down():
-    #form = ChampsForm()
-    # test = form.champs.choices
-    # print(test)
-    # if form.validate_on_submit():
-
-    #     for arg in request.form:
-    #         print(arg, request.form.getlist(arg))
-    #     choice = request.form['champs']
-    #     # query = request.form['recherche']
-    #     # n = int(request.form['nombre_mot_clé'])
-    #     # print(choice)
-    #     if choice == 'francais':
-    #         print('fr')
-    #         # combinaison = f"{choice}:{query}"
-    #         # converted = convert_str_to_dict(combinaison, n)
-    #         # print(converted)
-    #         # dicted = creation_dict(converted)
-
-    #         # print(dicted)
-
-    #         # benevoles = Benevole.objects(**dicted)
-
-    #         # for b in benevoles:
-    #         #     print(b.nom)
-    #     else:
-    #         print('autre')
-
-    # for arg in request.form:
-    #     print(arg, request.form.getlist(arg))
-
-    #     # return redirect(url_for('drop_down'))
-
-    # xd = request.form.copy()
-    # print(xd)
-
-    if request.method == "POST":
-        recherche = request.form['recherche']
-        print(recherche)
-        champs = request.form['champs']
-        print(champs)
-
-        for arg in request.form:
-            print(arg, request.form.getlist(arg))
-
-        print(request.form.getlist('recherche'))
-
-        return render_template('dropdown.html', title='Drop')
-    else:
-        return render_template('dropdown.html', title='DropDown')
 
 
 @app.route('/recherche8', methods=['GET', 'POST'])
---
+++ app/routes_not_used.py
@@ -0,0 +1,74 @@
+@app.route('/recherche', methods=['GET', 'POST'])
+def query():
+    query = QueryForm()
+    if query.validate_on_submit():
+        recherche = request.form['query_field']
+        # faire attention a ce que n soit bien un int
+        n = int(request.form['sliced'])
+
+        benevoles = query_function(recherche, n)
+        print(benevoles)
+
+        return render_template('recherche.html', title='Recherche', benevoles=benevoles)
+
+    return render_template('recherche_form.html', title='Recherche', query=query)
+
+
+@app.route('/drop_down', methods=['GET', 'POST'])
+def drop_down():
+    #form = ChampsForm()
+    # test = form.champs.choices
+    # print(test)
+    # if form.validate_on_submit():
+
+    #     for arg in request.form:
+    #         print(arg, request.form.getlist(arg))
+    #     choice = request.form['champs']
+    #     # query = request.form['recherche']
+    #     # n = int(request.form['nombre_mot_clé'])
+    #     # print(choice)
+    #     if choice == 'francais':
+    #         print('fr')
+    #         # combinaison = f"{choice}:{query}"
+    #         # converted = convert_str_to_dict(combinaison, n)
+    #         # print(converted)
+    #         # dicted = creation_dict(converted)
+
+    #         # print(dicted)
+
+    #         # benevoles = Benevole.objects(**dicted)
+
+    #         # for b in benevoles:
+    #         #     print(b.nom)
+    #     else:
+    #         print('autre')
+
+    # for arg in request.form:
+    #     print(arg, request.form.getlist(arg))
+
+    #     # return redirect(url_for('drop_down'))
+
+    # xd = request.form.copy()
+    # print(xd)
+
+    if request.method == "POST":
+        recherche = request.form['recherche']
+        print(recherche)
+        champs = request.form['champs']
+        print(champs)
+
+        for arg in request.form:
+            print(arg, request.form.getlist(arg))
+
+        print(request.form.getlist('recherche'))
+
+        return render_template('dropdown.html', title='Drop')
+    else:
+        return render_template('dropdown.html', title='DropDown')
+
+
+@app.route('/query')
+def test_query():
+    benevoles = Benevole.objects(nom__icontains="st")
+
+    return render_template('query.html', title='query_test', benevoles=benevoles)
