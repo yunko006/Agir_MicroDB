66ff577935690ba80bf4ec61063b9ca79685dee5
yunko006 <peyrachethomas@gmail.com>
2022-01-17 16:21:22 +0100

    update forms.py

--- app/forms.py
+++ app/forms.py
@@ -11,17 +11,20 @@ class LoginForm(FlaskForm):
 
 
 class BenevoleForm(FlaskForm):
-    id = StringField('ID', validators=[DataRequired()])
+    id = StringField("Numéro d'adhérent", validators=[DataRequired()])
     nom = StringField('Nom', validators=[DataRequired()])
     prenom = StringField('Prenom', validators=[DataRequired()])
-    #contact
+    # contact
     email = StringField('Email', validators=[DataRequired()])
     numero = StringField('Numéro de téléphone', validators=[DataRequired()])
-    #langues
+    # langues
     maternelle = StringField('Langue maternelle', validators=[DataRequired()])
-    autonome = StringField('Langues(s) avec un niveau autonome', validators=[DataRequired()])
-    notions = StringField('Langues(s) connues avec des notions', validators=[DataRequired()])
-    lu_parlé_écrit = StringField('Langue(s) lu parlé écrit', validators=[DataRequired()])
+    autonome = StringField(
+        'Langues(s) avec un niveau autonome', validators=[DataRequired()])
+    notions = StringField(
+        'Langues(s) connues avec des notions', validators=[DataRequired()])
+    lu_parlé_écrit = StringField(
+        'Langue(s) lu parlé écrit', validators=[DataRequired()])
     # secteurs et domaines
     secteurs = StringField('Secteurs', validators=[DataRequired()])
     domaines = StringField('Domaines', validators=[DataRequired()])
@@ -29,14 +32,16 @@ class BenevoleForm(FlaskForm):
     competences = StringField('Compétences', validators=[DataRequired()])
     # exp inter/bénévoles
     roles = StringField('Roles', validators=[DataRequired()])
-    expérience_internationale = StringField('Expérience Internationale', validators=[DataRequired()])
-    expérience_internationale_benevole = StringField('Expérience Internationale comme Bénévole', validators=[DataRequired()])
+    expérience_internationale = StringField(
+        'Expérience Internationale', validators=[DataRequired()])
+    expérience_internationale_benevole = StringField(
+        'Expérience Internationale comme Bénévole', validators=[DataRequired()])
     # submit
     submit = SubmitField('Enregistrer')
 
 
 class UpdateBenevoleForm(FlaskForm):
-    id = StringField('ID', validators=[DataRequired()])
+    id = StringField("Numéro d'adhérent", validators=[DataRequired()])
     field = StringField('Champs', validators=[DataRequired()])
     value = StringField('Valeur', validators=[DataRequired()])
     submit = SubmitField('Mettre à jour')
