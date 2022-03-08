d346cdd6a0078f74d30776548797a36867f8a53b
yunko006 <peyrachethomas@gmail.com>
2022-03-08 21:07:22 +0100

    Config en fonction des envs

--- config.py
+++ config.py
@@ -1,5 +1,16 @@
 import os
 
 
-class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+class Config:
+    DEBUG = False
+    TESTING = False
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SESSION_TYPE = 'mongodb'
+
+    if not SECRET_KEY:
+        raise ValueError("No SECRET_KEY set for the flask app")
+
+
+class DevelopmentConfig(Config):
+    BEBUG = True
+    TESTING = True
